2ad3f8619e1491cd86677851fcfbab6777a86365
Telme <telmaeugenio@gmail.com>
2021-11-05 15:57:40 -0300

    Adiciona Programa 3: retorna o maior de três numeros.

---
+++ fundamentos/bloco-4/dia-1-introducao-javascript/3.js
@@ -0,0 +1,11 @@
+//Retorna o maior de três números
+const a = 5;
+const b = 10;
+const c = 60;
+if (a > b && a > c) {
+    console.log(a);
+} else if (b > a && b > c) {
+    console.log(b);
+} else {
+    console.log(c);
+}
