96abf2064e4b70338864e1a151df5acd7ff4e1b0
Telme <telmaeugenio@gmail.com>
2021-11-05 14:41:06 -0300

    Adiciona arquivo scripts.js com práticas das aulas assincronas.

---
+++ fundamentos/bloco-4/dia-1-introducao-javascript/scripts.js
@@ -0,0 +1,15 @@
+const myName = "Telme";
+const birthCity = "Contagem";
+let birthYear = 1988;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "Belo Horizonte";
+console.log(birthCity);
+//birthYear foi definido como constante (const), logo não pode ser alterada.
+
