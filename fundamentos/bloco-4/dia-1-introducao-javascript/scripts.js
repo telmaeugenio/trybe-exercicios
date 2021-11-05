f02852e7d0843758f4a2472a4d945e8c4f117cae
Telme <telmaeugenio@gmail.com>
2021-11-05 15:31:40 -0300

    Parte 6: lógica para os dias da semana usando o operador OR.

--- fundamentos/bloco-4/dia-1-introducao-javascript/scripts.js
+++ fundamentos/bloco-4/dia-1-introducao-javascript/scripts.js
@@ -1,17 +1,6 @@
-const currentHour = 5;
-let message = '';
-if (currentHour >= 22) {
-    message = 'Não deveríamos comer nada, é hora de dormir';
-} else if (currentHour >= 18) {
-        message = 'Rango da noite, vamos jantar :D';
-} else if (currentHour >= 14) {
-    message = 'Vamos fazer um bolo para o café da tarde';
-} else if (currentHour >= 11) {
-    message = 'Hora do almoço!!!';
-} else if (currentHour >= 4) {
-    message = 'Hmmm, cheiro de café recém passado!'
+let weekDay = 'quarta-feira';
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+    console.log('Oba, mais um dia de aprenizado na Trybe');
 } else {
-    message = 'Não deveriamos comer nada, é hora de dormir!'
+    console.log('FINALMENTE, descanso merecido UwU')
 }
-
-console.log(message);
