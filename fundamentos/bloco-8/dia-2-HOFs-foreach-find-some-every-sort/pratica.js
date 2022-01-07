96b01c06cc4499a2026a2ae55b056c274ec71087
Telme <telmaeugenio@gmail.com>
2022-01-07 13:32:47 -0300

    Adiciona práticas do dia-2.

---
+++ fundamentos/bloco-8/dia-2-HOFs-foreach-find-some-every-sort/pratica.js
@@ -0,0 +1,91 @@
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+// Adicione seu código aqui
+emailListInData.forEach((showEmailList));
+
+// Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  // Adiciona seu código aqui
+  return numbers.find((number) => (number % 3 === 0) && (number % 5 === 0));
+};
+
+console.log(findDivisibleBy3And5());
+
+// Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  // Adicione seu código aqui:
+  return names.find((name) => name.length === 5);
+}
+
+console.log(findNameWithFiveLetters());
+
+// Utilize o find para encontrar a música com id igual a 31031685 , caso ela exista:
+
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ]
+  
+  function findMusic(id) {
+    // Adicione seu código aqui
+    return musicas.find((musica) => musica.id === '31031685')
+  };
+  
+  console.log(findMusic('31031685'))
+
+  // Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false , use some ;
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  //Adicione seu código aqui
+  return arr.some((nome) => nome === name);
+}
+
+console.log(hasName(names, 'Ana'))
+
+// Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false , use every ;
+
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 26 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  const verifyAges = (arr, minimumAge) => {
+    //Adicione seu código aqui
+    return arr.every((pessoa) => pessoa.age >= minimumAge );
+  }
+  
+  console.log(verifyAges(people, 18));
+
+  //Utilize a sort para ordenar o array pela idade das pessoas em ordem crescente 
+
+  const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  people.sort((personA, personB) => personA.age - personB.age);
+  
+  console.log(people);
