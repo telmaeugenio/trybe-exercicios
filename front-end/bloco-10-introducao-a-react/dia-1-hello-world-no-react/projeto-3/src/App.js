54ef07fe7717a6e8e59d839bcd5962f695f74d82
Telme <telmaeugenio@gmail.com>
2022-01-21 19:42:57 -0300

    Adiciona exercicio 3.

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/projeto-3/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header.jsx';
+import Content from './Content.jsx';
+import Footer from './Footer.jsx';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
